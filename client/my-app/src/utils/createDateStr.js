ca27458a9c386b36e936e6433639361ce4c60f10
MATANBENSHABAT <matanbs2000@gmail.com>
2022-11-03 15:30:02 +0200

    changed folder structure

--- client/my-app/src/utils/createDateStr.js
+++
@@ -1,3 +0,0 @@
-const createDateStr = (date)=>`${new Date(date).getDate()}.${new Date(date).getMonth()+1}`
-
-export default createDateStr
--- client/my-app/src/utils/minBid.js
+++
@@ -1,2 +0,0 @@
-const minBid = (bid) => (bid < 100 ? bid + 5 : bid + 50);
-export default minBid;
