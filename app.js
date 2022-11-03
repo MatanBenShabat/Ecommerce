d9ee2d7b35203dbc240b1d21ffd9f7bbd6db3f6d
MATANBENSHABAT <matanbs2000@gmail.com>
2022-11-03 16:51:55 +0200

    cors fixed2

--- app.js
+++ app.js
@@ -51,7 +51,8 @@ app.use(
 );
 
 // app.use(cors({credentials: true, origin: process.env.NODE_ENV === 'development'?'http://localhost:3000':process.env.SITE_URL}));
-app.use(cors({credentials: true, origin: process.env.NODE_ENV === 'development'?'https://rich-gray-bandicoot-slip.cyclic.app':process.env.SITE_URL}));
+app.use(cors({credentials: true, origin: 'https://rich-gray-bandicoot-slip.cyclic.app'}));
+// app.use(cors({credentials: true, origin: process.env.NODE_ENV === 'development'?'https://rich-gray-bandicoot-slip.cyclic.app':process.env.SITE_URL}));
 
 
 app.use(cookieParser());
