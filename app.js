6a0094a015dc32f0173a73ea0c4012c9e3a0a24d
MATANBENSHABAT <matanbs2000@gmail.com>
2022-11-06 15:26:49 +0200

    config

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const usersRoutes = require("./routes/api-users");
 const productsRoutes = require("./routes/api-products");
 const handle404 = require("./middlewares/handle404");
 const cors = require("cors");
+require('dotenv').config({ path: '.env' });
 
 const app = express();
 
@@ -55,7 +56,6 @@ app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 
 
 app.use(cookieParser());
-
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
