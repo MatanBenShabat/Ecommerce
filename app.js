6d05e3e5127254eb8809f9bb052944036e0919d6
MATANBENSHABAT <matanbs2000@gmail.com>
2022-11-06 17:18:59 +0200

    cors

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const xss = require("xss-clean");
 const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
 
-
 const globalErrorHandler = require("./controllers/errorController");
 const usersRoutes = require("./routes/api-users");
 const productsRoutes = require("./routes/api-products");
@@ -52,8 +51,17 @@ app.use(
 
 // app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(cors({credentials: true}));
-// app.use(cors({credentials: true, origin: process.env.NODE_ENV === 'development'?'https://rich-gray-bandicoot-slip.cyclic.app':process.env.SITE_URL}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept,Authorization,token,origin"
+  );
 
+  next();
+});
+// app.use(cors({credentials: true, origin: process.env.NODE_ENV === 'development'?'https://rich-gray-bandicoot-slip.cyclic.app':process.env.SITE_URL}));
 
 app.use(cookieParser());
 
