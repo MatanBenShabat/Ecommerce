0a19e9367a751aed1b6fc00b54e7b1aa9fc19247
MATANBENSHABAT <matanbs2000@gmail.com>
2022-11-07 23:21:41 +0200

    samesite

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    sameSite: "Lax",
+    sameSite: "None",
   };
   if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
   res.cookie("jwt", token, cookieOptions);
@@ -49,7 +49,7 @@ exports.logout = (req, res, next) => {
   const cookieOptions = {
     expires: new Date(Date.now()),
     httpOnly: true,
-    sameSite: true,
+    sameSite: "None",
   };
   if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
   res.cookie("jwt", null, cookieOptions);
